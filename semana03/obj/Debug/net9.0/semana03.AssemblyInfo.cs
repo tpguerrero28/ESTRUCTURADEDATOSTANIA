8de966d59a0bae8219a55ebae3c1aad074d495c6
TANIA GUERRERO <tp.guerrerom@uea.edu.ec>
2025-06-15 16:32:49 -0500

    Tareasemana03
    Tareasemana03

--- semana03/obj/Debug/net9.0/semana03.AssemblyInfo.cs
+++ semana03/obj/Debug/net9.0/semana03.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("semana03")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f75b080cc0fc322dfa3d254311eba70e1cd7201")]
 [assembly: System.Reflection.AssemblyProductAttribute("semana03")]
 [assembly: System.Reflection.AssemblyTitleAttribute("semana03")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
