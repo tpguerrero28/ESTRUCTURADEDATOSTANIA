da3bd34cb122c698ccad2df0497676064288d6d2
TANIA GUERRERO <tp.guerrerom@uea.edu.ec>
2025-06-27 23:18:52 -0500

    semana05
    ejercicio01

---
+++ semana05/ejercicio01/Program.cs
@@ -0,0 +1,8 @@
+﻿class Ejercicio2 {
+    static void Main() {
+        string[] asignaturas = { "ESTRUCTURA DE DATOS", "ADMINISTRACION", "FUNDAMENTOS", "INSTALACIONES", "METODOLOGIA" };
+        foreach (string asignatura in asignaturas) {
+            Console.WriteLine($"YO ESTUDIO {asignatura}");
+        }
+    }
+}
