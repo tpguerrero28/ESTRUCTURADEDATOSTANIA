e82e03cb9f84c1e7969b04ada0b2213745f2ae40
TANIA GUERRERO <tp.guerrerom@uea.edu.ec>
2025-06-27 22:08:54 -0500

    semana05
    yoestudioasignaturas

---
+++ semana05/ejercicio01.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Ejercicio2
+{
+    static void Main()
+    {
+        List<string> asignaturas = new List<string> { "ESTRUCTURA DE DATOS", "ADMINISTRACION", "INSTALACION", "METODOLOGIA", "FUNDAMENTOS" };
+
+        foreach (string asignatura in asignaturas)
+        {
+            Console.WriteLine("Yo estudio " + asignatura);
+        }
+    }
+}
+
