65ea3fcf1be6ec4c4b5337eb3533b84ccc3dd48b
TANIA GUERRERO <tp.guerrerom@uea.edu.ec>
2025-07-12 18:57:25 -0500

    semana07
    ejercicio02Torres de Hanoi con pilas

--- semana07/ejercicio02/Program.cs
+++ semana07/ejercicio02/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, semana07 ejercicio02 Torres de Hanoi con pilas!");
+﻿using System;
+using System.Collections.Generic;
+
+class TorreHanoi
+{
+    static void MoverDiscos(int n, Stack<int> origen, Stack<int> destino, Stack<int> auxiliar, string nombreOrigen, string nombreDestino, string nombreAuxiliar)
+    {
+        if (n == 1)
+        {
+            int disco = origen.Pop();
+            destino.Push(disco);
+            Console.WriteLine($"Mover disco {disco} de {nombreOrigen} a {nombreDestino}");
+            return;
+        }
+
+        MoverDiscos(n - 1, origen, auxiliar, destino, nombreOrigen, nombreAuxiliar, nombreDestino);
+        MoverDiscos(1, origen, destino, auxiliar, nombreOrigen, nombreDestino, nombreAuxiliar);
+        MoverDiscos(n - 1, auxiliar, destino, origen, nombreAuxiliar, nombreDestino, nombreOrigen);
+    }
+
+    static void Main()
+    {
+        Console.Write("Ingrese el número de discos: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Stack<int> origen = new Stack<int>();
+        Stack<int> destino = new Stack<int>();
+        Stack<int> auxiliar = new Stack<int>();
+
+        for (int i = n; i >= 1; i--)
+            origen.Push(i);
+
+        Console.WriteLine("Secuencia de movimientos:");
+        MoverDiscos(n, origen, destino, auxiliar, "Origen", "Destino", "Auxiliar");
+    }
+}
