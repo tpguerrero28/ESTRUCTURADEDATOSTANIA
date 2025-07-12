0f73e079bc71588556f8e3b05d6cc378857d875b
TANIA GUERRERO <tp.guerrerom@uea.edu.ec>
2025-07-12 18:45:08 -0500

    semana07
    ejercicio01Verificación de paréntesis balanceados

--- semana07/ejercicio01/Program.cs
+++ semana07/ejercicio01/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, semana07 ejercicioo1 Verificación de paréntesis balanceados!");
+﻿using System;
+using System.Collections.Generic;
+
+class BalanceoParentesis
+{
+    static bool EstaBalanceado(string expresion)
+    {
+        Stack<char> pila = new Stack<char>();
+        foreach (char c in expresion)
+        {
+            if (c == '(' || c == '[' || c == '{')
+                pila.Push(c);
+            else if (c == ')' || c == ']' || c == '}')
+            {
+                if (pila.Count == 0)
+                    return false;
+
+                char tope = pila.Pop();
+                if ((c == ')' && tope != '(') ||
+                    (c == ']' && tope != '[') ||
+                    (c == '}' && tope != '{'))
+                    return false;
+            }
+        }
+        return pila.Count == 0;
+    }
+
+    static void Main()
+    {
+        Console.Write("Ingrese una expresión matemática: ");
+        string expresion = Console.ReadLine();
+
+        if (EstaBalanceado(expresion))
+            Console.WriteLine("Fórmula balanceada.");
+        else
+            Console.WriteLine("Fórmula NO balanceada.");
+    }
+}
